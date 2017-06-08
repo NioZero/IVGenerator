e5da071b199adab3fa4afd3ab74ed06e19cd085c
NioZero <ghidalgo@frostdisk.com>
2017-06-08 08:57:37 -0400

    Basic App to Generate IV

---
+++ IVGenerator/Source/IVGenerator.App/FormMain.cs
@@ -0,0 +1,39 @@
+﻿using IVGenerator.Encryption;
+using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace IVGenerator.App
+{
+    public partial class FormMain : Form
+    {
+        #region properties
+        #endregion
+
+        #region initialization
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region methods
+        #endregion
+
+        #region events
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var vector = RijndaelEncryptorHelper.GenerateVector();
+
+            var vectorString = vector.ToList().Select(n => string.Format("0x{0:x2}", n));
+
+            textBox1.Text = string.Join(", ", vectorString);
+        }
+
+        #endregion
+    }
+}
---
+++ IVGenerator/Source/IVGenerator.Encryption/RijndaelEncryptorHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IVGenerator.Encryption
+{
+    public static class RijndaelEncryptorHelper
+    {
+        public static byte[] GenerateVector()
+        {
+            using (var des = new RijndaelManaged())
+            {
+                des.GenerateIV();
+
+                return des.IV;
+            }
+        }
+    }
+}
